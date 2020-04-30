13f9ea0c3e0d0db82d6ed70d80e131d1b41b262b
Edu <eparra@digitalhouse.com>
2020-04-30 20:15:16 -0300

    se agrega Navigation view

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,18 +1,35 @@
 package com.example.recyclerview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements RecyclerViewFragment.RecyclerViewFragmentListener {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements RecyclerViewFragment.RecyclerViewFragmentListener, NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navigationView = findViewById(R.id.navigationView);
+        drawerLayout = findViewById(R.id.drawerLayout);
+
+        navigationView.setNavigationItemSelectedListener(this);
 
         //pegar el fragment.
         RecyclerViewFragment recyclerViewFragment = new RecyclerViewFragment();
@@ -20,10 +37,34 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewFragm
 
     }
 
-    private void pegarFragment(RecyclerViewFragment recyclerViewFragment) {
+    private void setNavigationViewListener() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.menuInicio:
+                        Toast.makeText(MainActivity.this, "Presionaron inicio", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.menuFavorito:
+                        FavoritoFragment favoritoFragment = new FavoritoFragment();
+                        pegarFragment(favoritoFragment);
+                        drawerLayout.closeDrawers();
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this, "En construccion", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return true;
+            }
+        });
+    }
+
+    private void pegarFragment(Fragment fragment) {
         FragmentManager supportFragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.activityMainContenedorFragment,recyclerViewFragment);
+        fragmentTransaction.replace(R.id.activityMainContenedorFragment,fragment);
         fragmentTransaction.commit();
     }
 
@@ -39,5 +80,24 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewFragm
         unIntent.putExtras(bundle);
 
         startActivity(unIntent);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuInicio:
+                Toast.makeText(MainActivity.this, "Presionaron inicio", Toast.LENGTH_SHORT).show();
+                drawerLayout.closeDrawers();
+                break;
+            case R.id.menuFavorito:
+                FavoritoFragment favoritoFragment = new FavoritoFragment();
+                pegarFragment(favoritoFragment);
+                drawerLayout.closeDrawers();
+                break;
+            default:
+                Toast.makeText(MainActivity.this, "En construccion", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
